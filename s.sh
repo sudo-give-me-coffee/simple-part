5e0bf610fc4b8f3fb5071f4dd737ac3b7715c033
Natanael <natanaelgplay@gmail.com>
2021-10-29 20:15:09 -0300

    Update s.sh

--- s.sh
+++ s.sh
@@ -2,27 +2,27 @@
 
 function erase-diskpartition-table(){
   echo -e "g
-           w" | fdisk ${device}
+           w" | fdisk "${device}"
 }
 
 function format-partition(){
   [ "${2}" = "swap"  ] && {
-    echo y | mkswap  ${device}${1}
+    echo y | mkswap  "${device}""${1}"
     return ${?}
   }
   
   [ "${2}" = "btrfs" ] && {
-    mkfs.btrfs -f ${device}${1}
+    mkfs.btrfs -f "${device}""${1}"
     return ${?}
   }
   
-  echo y | mkfs.${2} ${device}${1}
+  echo y | mkfs."${2}" "${device}""${1}"
   return ${?}
 }
 
 function delete-partition() {
   echo "d
-        ${1}" | fdisk ${device}
+        ${1}" | fdisk "${device}"
 }
 
 function create-partition(){
@@ -33,22 +33,22 @@ function create-partition(){
         
         ${partition_size}
         y
-        w" | fdisk ${device}
+        w" | fdisk "${device}"
 }
 
-device=$(echo ${1} | cut -c 10-); shift
+device=$(echo "${1}" | cut -c 10-); shift
 [ ! -f "${device}" ] && {
   echo "Fatal error: '${device}' not found"
 }
 
-disk_size_bytes=$(echo "p" | fdisk ${device} | grep -o ",.*bytes" | cut -d' ' -f2)
+disk_size_bytes=$(echo "p" | fdisk "${device}" | grep -o ",.*bytes" | cut -d' ' -f2)
 disk_size_gigabytes=$(echo "${disk_size_bytes}"/1024/1024/1024|bc)
-disk_size_sectors=$(echo "p" | fdisk ${device} | grep -o "bytes.*sectors$" | cut -d' ' -f2)
+disk_size_sectors=$(echo "p" | fdisk "${device}" | grep -o "bytes.*sectors$" | cut -d' ' -f2)
 
 [ "${1}" = "--erase-partition-table" ] && {
   erase-diskpartition-table
   shift
-  partprobre ${device}
+  partprobre "${device}"
 }
 
 for arg in "${@}"; do
@@ -59,27 +59,27 @@ for arg in "${@}"; do
   echo "${arg}" grep -q ^"--delete-partition=" && {
     number=$(echo "${arg}" | cut -c 20-)
     
-    format-partition ${number}
+    format-partition "${number}"
   }
   echo "${arg}" | grep -q ^"--create-partition=" && {
     params=$(echo "${arg}" | cut -c 20-)
-    number=$(echo ${params} | cut -d: -f1)
-    size=$(echo ${params} | cut -d: -f2)
-    format=$(echo ${params} | cut -d: -f3)
+    number=$(echo "${params}" | cut -d: -f1)
+    size=$(echo "${params}" | cut -d: -f2)
+    format=$(echo "${params}" | cut -d: -f3)
     
-    create-partition ${number} ${size} ${format}
+    create-partition "${number}" "${size}" "${format}"
   }
   echo "${arg}" grep -q ^"--format-partition=" && {
     params=$(echo "${arg}" | cut -c 20-)
-    number=$(echo ${params} | cut -d: -f1)
-    format=$(echo ${params} | cut -d: -f2)
+    number=$(echo "${params}" | cut -d: -f1)
+    format=$(echo "${params}" | cut -d: -f2)
     
-    format-partition ${number} ${format}
+    format-partition "${number}" "${format}"
   }
 done
 
 
 [ -f "${target_partition}" ] && {
   mkdir -p /target
-  mount -o rw ${target_partition}  /target
+  mount -o rw "${target_partition}"  /target
 }
